9f463451d1f16e8da0d6549a1e8ea6729a3c0173
Pablo Jesús Reyes <72435946+PabloJesusReyes@users.noreply.github.com>
2020-12-14 14:37:26 +0100

    subido examen

---
+++ javascripts/funciones.js
@@ -0,0 +1,12 @@
+
+/******** javascript de topnav responsive */
+
+/* para la barra de nevegación con menu hamburguesa */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
